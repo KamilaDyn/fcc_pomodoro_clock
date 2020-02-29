590eb7537afc0535cb858c5faf49b798bb66bc7a
unknown <kamila0990@gmail.com>
2020-02-29 22:43:48 +0000

    Updates

---
+++ precache-manifest.51eb1555e0966f87b9a38333345cf16f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a286c645ab5cc8188e6669ea24b1036",
+    "url": "/fcc_pomodoro_clock/index.html"
+  },
+  {
+    "revision": "b3f703cd577ae113f776",
+    "url": "/fcc_pomodoro_clock/static/css/main.0cdcf64c.chunk.css"
+  },
+  {
+    "revision": "2e90760c531bffb4c35f",
+    "url": "/fcc_pomodoro_clock/static/js/2.47a4f3e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fcc_pomodoro_clock/static/js/2.47a4f3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f703cd577ae113f776",
+    "url": "/fcc_pomodoro_clock/static/js/main.b859a8b5.chunk.js"
+  },
+  {
+    "revision": "92d23d964912cd2017f9",
+    "url": "/fcc_pomodoro_clock/static/js/runtime-main.78a4f953.js"
+  },
+  {
+    "revision": "fff9f0194725f5d0c7c9c02d2638d953",
+    "url": "/fcc_pomodoro_clock/static/media/play-multimedia-outlined-button-circle-svgrepo-com.fff9f019.svg"
+  },
+  {
+    "revision": "7e62db55727d81db18d8eef5d891953d",
+    "url": "/fcc_pomodoro_clock/static/media/refresh-svgrepo-com.7e62db55.svg"
+  },
+  {
+    "revision": "ba199f9c284e33d57667878570962b66",
+    "url": "/fcc_pomodoro_clock/static/media/tomato-svgrepo-com.ba199f9c.svg"
+  }
+]);
